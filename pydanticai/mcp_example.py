f60e8afb00ec078ffa01a45495b55fcd066612f9
Alonso Astroza Tagle <alonsoastroza@gmail.com>
2025-04-03 21:41:29 -0300

    refactor: pydanticai folder

--- pydanticai/mcp_example.py
+++ pydanticai/mcp_example.py
@@ -20,7 +20,6 @@ async def main():
     async with agent.run_mcp_servers():
         result = await agent.run('Can you summarize my last 10 whatsapp messages with my boss (Loreto Bravo)?')
     print(result.data)
-    #> There are 9,208 days between January 1, 2000, and March 18, 2025.
 
 if __name__ == "__main__":
     asyncio.run(main())
